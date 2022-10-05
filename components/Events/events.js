73cac7cedeeac6b10c16a374fbe72e1a65fcbc55
Somesh Kumar <kumarsomesh.unos@gmail.com>
2022-10-05 20:19:18 +0300

    y

---
+++ components/Events/events.js
@@ -0,0 +1,70 @@
+const events = [
+  {
+    image:
+      'https://images.prismic.io/ieeemuj/c05e54f3-866d-4d72-a8dc-adaff72919a7_WhatsApp+Image+2022-10-05+at+03.32.48+%283%29.jpeg?auto=compress,format',
+    type: 'Hackathon',
+    title: 'What the Hack',
+    desc: 'A Web3 - Blockchain based project that lets you transfer ETHereums from your wallet to a reciever.',
+    auth_name: 'IEEE SB MUJ',
+    status: 'completed',
+    date: 'Sept 09, 2022 - 19:00hrs',
+    redirect: '7',
+  },
+  {
+    image:
+      'https://images.prismic.io/ieeemuj/efdab0b8-71d1-46b4-b99c-1ee7147b78f8_WhatsApp+Image+2022-10-05+at+03.33.58.jpeg?auto=compress,format',
+    type: 'Seminar',
+    title: 'Roadmap to DSA',
+    desc: 'A Web3 - Blockchain based project that lets you transfer ETHereums from your wallet to a reciever.',
+    auth_name: 'IEEE SB MUJ',
+    status: 'completed',
+    date: 'Sept 03, 2022 - 14:00hrs',
+    redirect: '9',
+  },
+  {
+    image:
+      'https://images.prismic.io/ieeemuj/0215c45f-c436-41ed-8f0d-6cb7fdacbed3_WhatsApp+Image+2022-10-05+at+03.32.48+%282%29.jpeg?auto=compress,format',
+    type: 'Designathon',
+    title: 'Re-Branded',
+    desc: 'A Web3 - Blockchain based project that lets you transfer ETHereums from your wallet to a reciever.',
+    auth_name: 'IEEE CS MUJ',
+    status: 'completed',
+    date: 'Aug 13, 2022 - 17:00hrs',
+    redirect: '6',
+  },
+  {
+    image:
+      'https://images.prismic.io/ieeemuj/c2c2b6bd-6edc-4a51-ad78-ea41a2728612_WhatsApp+Image+2022-10-05+at+03.32.48.jpeg?auto=compress,format',
+    type: 'Webinar',
+    title: 'Monopoly',
+    desc: 'A Web3 - Blockchain based project that lets you transfer ETHereums from your wallet to a reciever.',
+    auth_name: 'IEEE SB MUJ',
+    status: 'completed',
+    date: 'Aug 08, 2022 - 17:00hrs',
+    redirect: '10',
+  },
+  
+  {
+    image:
+      'https://images.prismic.io/ieeemuj/f90af906-c459-463b-bd24-6fed4a0d7677_WhatsApp+Image+2022-10-05+at+03.33.58+%281%29.jpeg?auto=compress,format',
+    type: 'Hackathon',
+    title: 'HACKERZSTREET',
+    desc: 'A Web3 - Blockchain based project that lets you transfer ETHereums from your wallet to a reciever.',
+    auth_name: 'IEEE WIE MUJ',
+    status: 'completed',
+    date: 'July 09, 2022 - 16:30hrs',
+  },
+  {
+    image:
+      'https://images.prismic.io/ieeemuj/f3b9b36f-b840-489e-bd8a-1e1f197b4e3f_WhatsApp+Image+2022-10-05+at+03.32.47.jpeg?auto=compress,format',
+    type: 'Seminar',
+    title: 'Cyberift',
+    desc: 'A Web3 - Blockchain based project that lets you transfer ETHereums from your wallet to a reciever.',
+    auth_name: 'IEEE SB MUJ',
+    status: 'completed',
+    date: 'Jun 22, 2022 - 12:15hrs',
+    redirect: '8',
+  },
+  ];
+  
+  export default events;
